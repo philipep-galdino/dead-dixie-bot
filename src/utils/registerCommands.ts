7794a80114a6929d87d3bd316db2d4c245753b10
Philipe <philipep.galdino@gmail.com>
2023-05-01 20:21:31 -0300

    fix: omg probably the issue was that .js not being read...

--- src/utils/registerCommands.ts
+++ src/utils/registerCommands.ts
@@ -6,7 +6,7 @@ import path from 'path';
 export function registerCommands(client: ExtendedClient, commandsFolder: string) {
   client.commands = new Collection();
 
-  const commandFiles = fs.readdirSync(path.join(__dirname, commandsFolder)).filter((file) => file.endsWith('.ts'));
+  const commandFiles = fs.readdirSync(path.join(__dirname, commandsFolder)).filter((file) => file.endsWith('.ts') || file.endsWith('.js'));
 
   for (const file of commandFiles) {
     const command: Command = require(path.join(__dirname, commandsFolder, file)).default;
